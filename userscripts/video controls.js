c170444939e22336b534290b013deb444ea04f59
RJK-Engineering <rjklinkhamer@gmail.com>
2022-02-12 17:21:50 +0100

    Update video controls.js

--- userscripts/video controls.js
+++ userscripts/video controls.js
@@ -6,25 +6,35 @@
 // ==/UserScript==
 
 v = document.getElementsByTagName("video")[0];
+
 if (!v) {
     console.log("no video element");
 } else {
+    sync = document.URL.startsWith("https://zst-cdp.triple-it.nl/missed");
+    if (v.volume == 1) v.volume = .5;
     keys = {
-        c: function() { v.fastSeek(v.currentTime-2) },
-        v: function() { v.fastSeek(v.currentTime+2) },
-        a: function() { v.fastSeek(v.currentTime-5) },
-        s: function() { v.fastSeek(v.currentTime+5) },
-        z: function() { v.fastSeek(v.currentTime-30) },
-        x: function() { v.fastSeek(v.currentTime+30) },
+        c: function() { seek(-2) },
+        v: function() { seek(2) },
+        a: function() { seek(-5) },
+        s: function() { seek(5) },
+        z: function() { seek(-30) },
+        x: function() { seek(30) },
         q: function() { seekToPrevFrame() },
         w: function() { seekToNextFrame() },
-        // : function() { v.volume += .1 },
         m: function() { v.muted = !v.muted },
     };
     document.onkeydown = function (e) {
         keys[e.key]();
     }
     console.log("video controls enabled");
+
+    function seek(t) {
+        v.fastSeek(v.currentTime+t);
+        if (sync)
+                setTimeout(function(){
+                        v.seekToNextFrame();
+                }, 1000);
+    }
 
     function seekToNextFrame() {
         if (v.frameDuration)
@@ -46,6 +56,7 @@ if (!v) {
         let t = v.currentTime;
         v.seekToNextFrame().then(function () {
             v.frameDuration = v.currentTime - t;
+            console.log("frame duration: " + v.frameDuration);
             if (callback) callback();
         });
     }
